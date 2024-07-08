c94fd091654256aed0c50712b125e4b0b238b5cf
Bear M <bear.mistele@gmail.com>
2024-07-08 16:31:36 -0700

    .env additions and route tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,13 +4,22 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
-});
+  // find all tags be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag, as: 'tag_product' }]
+    });
+    res.status(200).json(tagData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+}); 
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+
 });
 
 router.post('/', (req, res) => {
